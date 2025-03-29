2ab07cd2670825dac5d4ce134fa762c6000fedbd
csnizik <csnizik@tulane.edu>
2025-03-28 20:58:07 -0500

    initialize lint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['react-app', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+}
